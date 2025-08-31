b95af697260aa887d8635e2b60198d0c8fb842ce
umamaheshdega@umadega <mahesh445667@gmail.com>
2025-08-31 14:46:30 +0530

    shell scripting

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,5 +4,5 @@ PERSON1=RAMESH
 PERSON2=SURESH
 echo "$PERSON1::HI $PERSON2"
 echo "$PERSON2:: HI $PERSON1"
-echo $PERSON1::how are you $PERSON2"
-echo "$PERSON2::i am fine.how about you ?"
+echo "$PERSON1::how are you $PERSON2"
+echo "$PERSON2::i am fine.how about you $PERSON1"
