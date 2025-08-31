74a1b64aa0479584b8cbb831c6715f557584350b
umamaheshdega@umadega <mahesh445667@gmail.com>
2025-08-31 14:55:55 +0530

    shell scripting

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "ENTER YOUR USERNAME::"
-read -s USERNAME
+read  USERNAME
 echo "username entered: $USERNAME"
 echo "PLEASE ENTER YOUR PASSWORD::"
 read -s PASSWORD
+echo "password entered: $PASSWORD
