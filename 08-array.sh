69333af0637c0657a0612a7d5981b75158adcc32
umamaheshdega@umadega <mahesh445667@gmail.com>
2025-09-01 10:34:56 +0530

    shell script

--- 08-array.sh
+++ 08-array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MOVIES=("PUSPHA", "RRR", "DEVARA")
+MOVIES=("PUSPHA" "RRR" "DEVARA")
 
 echo "FIRST MOVIE:${MOVIES[0]}"
-echo "SECOND MOVIE:${MOVIE[1]}"
-echo "THRID MOVIE:${MOVIE[2]}"
+echo "SECOND MOVIE:${MOVIES[1]}"
+echo "THRID MOVIE:${MOVIES[2]}"
 
 echo "ALL MOVIES ARE : ${MOVIES[@]}"
