b32290221bb627e989991aaf21d87a9431b06847
umamaheshdega@umadega <mahesh445667@gmail.com>
2025-09-01 11:27:23 +0530

    shell script

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-if [ USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then
 echo "ERROR:you must have sudo access to execute script"
 exit 1
@@ -23,7 +23,7 @@ else
 echo "mysql already installed"
 fi  
 
-if [ USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then 
 echo "ERROR:YOU MUST HAVE SUDO ACCESS TO EXECUTE SCRIPT"
 exit 1
