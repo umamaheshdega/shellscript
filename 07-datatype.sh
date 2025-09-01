1b29568776a191459696ee9c6f66140ea5895962
umamaheshdega@umadega <mahesh445667@gmail.com>
2025-09-01 10:25:20 +0530

    shell script

--- 07-datatype.sh
+++ 07-datatype.sh
@@ -6,4 +6,4 @@ NUMBER=$2
 timestamp=$(data)
 echo script executed at:$timestamp
 SUM=$(($NUMBER1+$NUMBER2))
-echo "SUM OF $NUMBER1 AND $NUMBER2 is: $SUM
+echo "SUM OF $NUMBER1 AND $NUMBER2 is: $SUM"
